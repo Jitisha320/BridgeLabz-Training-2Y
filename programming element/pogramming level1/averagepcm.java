f7e1796d348a1518711d4a9a67b2cfefb9b5cc77
Jitisha Agarwal <jitishaagarwal10@gmail.com>
2025-09-23 11:45:05 +0530

    Added: Programming elements practice problems

---
+++ programming element/pogramming level1/averagepcm.java
@@ -0,0 +1,12 @@
+
+
+public class averagepcm {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        int avg = (maths + physics + chemistry)/3;
+        System.out.println("the average of three subjects is =  " + avg);
+    }
+}
---
+++ programming element/pogramming level1/kmtomile.java
@@ -0,0 +1,10 @@
+public class kmtomile {
+     public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        int avg = (maths + physics + chemistry)/3;
+        System.out.println("the average of three subjects is =  " + avg);
+    }
+}
