5ae5bf44194e8f0284e45465cc46a831f2a50b2d
Jitisha Agarwal <jitishaagarwal10@gmail.com>
2025-09-23 11:40:50 +0530

    Added: Java fundamentals practice problems

---
+++ programming fundamental/java2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class java2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Number1:");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter Number2:");
+        int num2 = sc.nextInt();
+
+        System.out.printf("SUM: %d", num1 + num2);
+
+        sc.close(); // good practice to close Scanner
+    }
+}
